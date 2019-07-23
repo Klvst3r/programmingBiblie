73bc4474be5574341cdc1e0233cda86b93d44d58
Klvst3r <klvst3r@gmail.com>
2019-07-23 15:48:05 -0500

    Beginning with variables

---
+++ variables.py
@@ -0,0 +1,18 @@
+#Cook your dish here
+
+var1 = 500
+var2 = 45.68
+var3 = "Hello World"
+
+a = b = c = 7
+print(var1)
+print(var2)
+print (b)
+print (a)
+print (c)
+print(var3[0])
+print(var3)
+print(var3[0:4])
+print(var3[1:])
+
+
