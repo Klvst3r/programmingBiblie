ec31e778fa07ef9070b8fbeed81406f50d3b7f58
Klvst3r <klvst3r@gmail.com>
2019-07-23 19:39:53 -0500

    Three - Tuples

---
+++ tuples.py
@@ -0,0 +1,20 @@
+
+awesomeList = ['Hello', 45, 'World', 4.5]
+
+awesomeList[3] = 6
+
+print (awesomeList[3])
+
+
+
+awesomeTuple = ('Hello', 45, 'World', 4.5)
+
+
+print (awesomeTuple)
+print (awesomeTuple[0])
+print (awesomeTuple[3])
+print (awesomeTuple[0:2])
+
+#awesomeTuple = 6
+
+print (awesomeTuple[3])
