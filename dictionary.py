bc61631ecf8d720fef73271a658993f0ad992b2f
Klvst3r <klvst3r@gmail.com>
2019-07-30 11:59:39 -0500

    Working with Dictioraries

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+
+dictionary = {}
+dictionary[1] = "Item1"
+dictionary[2] = "Item2"
+dictionary["Hello"] = "Item3"
+
+
+print(dictionary[1])
+print(dictionary[2])
+print(dictionary["Hello"])
+print(dictionary)
+print(dictionary.values())
+print(dictionary.keys())
+
+newDictionary = {1:"Item1", 2:"Item2", "Hello":"Item3"}
+
+print(newDictionary)
